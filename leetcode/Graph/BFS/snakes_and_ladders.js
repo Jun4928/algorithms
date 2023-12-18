507599fe64cb7f8627df8a56b4ec07445768537a
Jun4928 <jun.choi.4928@gmail.com>
2023-12-18 16:17:44 +0100

    feat: snakes and ladders

---
+++ leetcode/Graph/BFS/snakes_and_ladders.js
@@ -0,0 +1,8 @@
+/**
+ * https://leetcode.com/problems/snakes-and-ladders/description/
+ * 909. Snakes and Ladders
+ *
+ * @param {number[][]} board
+ * @return {number}
+ */
+var snakesAndLadders = function (board) {}
