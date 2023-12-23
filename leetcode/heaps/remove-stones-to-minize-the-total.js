5cc4a4adc12e024d6133d7c1c250b803c9ec97bf
Jun4928 <jun.choi.4928@gmail.com>
2023-12-23 17:38:12 +0100

    feat: 1962: min stone sum

---
+++ leetcode/heaps/remove-stones-to-minize-the-total.js
@@ -0,0 +1,28 @@
+/**
+https://leetcode.com/problems/remove-stones-to-minimize-the-total/description/
+1962. Remove Stones to Minimize the Total
+ * @param {number[]} piles
+ * @param {number} k
+ * @return {number}
+ */
+var minStoneSum = function (piles, k) {
+  const maxHeap = new PriorityQueue({ compare: (a, b) => (a > b ? -1 : 1) })
+  piles.forEach(n => maxHeap.enqueue(n))
+
+  let count = 0
+  while (count < k) {
+    const max = maxHeap.dequeue()
+    maxHeap.enqueue(Math.ceil(max / 2))
+    count++
+  }
+
+  return maxHeap.toArray().reduce((a, b) => a + b, 0)
+}
+
+/**
+ * 
+  Time: O(N + K * logN)
+    - to convert piles into maxHeap
+    - perform K operations and each costs O(logN)
+  Space: O(N): heap's length
+ */
