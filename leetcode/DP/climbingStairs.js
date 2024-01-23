c64011ccf2782ec3b9808092ddbc430870296dda
Jun4928 <jun.choi.4928@gmail.com>
2024-01-23 10:26:23 +0100

    feat: 70 climbing stairs

--- leetcode/DP/climbingStairs.js
+++ leetcode/DP/climbingStairs.js
@@ -1,45 +1,23 @@
 /**
+https://leetcode.com/problems/climbing-stairs/
+70. Climbing Stairs
  * @param {number} n
  * @return {number}
  */
-// Time: O(n), Space: O(n) => dp array
-var climbStairsDP = function (n) {
-  // Staris(1) = 1
-  // Stairs(2) = 2
-  const stairsDP = [0, 1, 2]
-  if (stairsDP[n]) return stairsDP[n]
+var climbStairs = function (n) {
+  let memo = Array(n).fill(-1)
+  const DP = steps => {
+    if (steps <= 0) {
+      return 1
+    }
 
-  let stair = 3
-  while (stair <= n) {
-    const current = stairsDP[stair - 1] + stairsDP[stair - 2]
-    stairsDP.push(current)
-    stair += 1
+    if (memo[steps] > -1) {
+      return memo[steps]
+    }
+
+    memo[steps] = DP(steps - 1) + DP(steps - 2)
+    return memo[steps]
   }
 
-  return stairsDP[n]
+  return DP(n - 1)
 }
-
-console.log(climbStairsDP(3))
-console.log(climbStairsDP(4))
-console.log(climbStairsDP(100))
-
-// Time: O(n), Space: O(1)
-var climbStairsFib = function (n) {
-  if (n === 1) return 1
-  let first = 1
-  let second = 2
-
-  let stair = 3
-  while (stair <= n) {
-    let thrid = first + second
-    first = second
-    second = thrid
-    stair += 1
-  }
-
-  return second
-}
-
-console.log(climbStairsFib(3))
-console.log(climbStairsFib(4))
-console.log(climbStairsFib(100))
