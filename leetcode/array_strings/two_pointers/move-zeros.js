d39d30beabfee6c4d7a1196089984dfa977e0228
Jun4928 <jun.choi.4928@gmail.com>
2024-02-06 11:29:03 +0100

    feat: 283 move zeros

---
+++ leetcode/array_strings/two_pointers/move-zeros.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+
+const swap = (left, right, nums) => {
+  const tmp = nums[right]
+  nums[right] = nums[left]
+  nums[left] = tmp
+}
+
+var moveZeroes = function (nums) {
+  let nonZero = 0 // non-zero
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      swap(i, nonZero, nums)
+      nonZero++ // 바꿀때마다 한칸씩 증가
+    }
+  }
+}
