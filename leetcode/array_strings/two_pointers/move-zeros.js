b510518b20273fbeb22b905d861d79f1a8f16b47
Jun4928 <jun.choi.4928@gmail.com>
2024-02-06 11:29:06 +0100

    feat: 283 move zeros

--- leetcode/array_strings/two_pointers/move-zeros.js
+++ leetcode/array_strings/two_pointers/move-zeros.js
@@ -1,4 +1,6 @@
 /**
+ * https://leetcode.com/problems/move-zeroes/description/
+ * 283. Move Zeroes
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
