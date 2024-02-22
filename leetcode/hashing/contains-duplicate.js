02fed445c7823de5becd9c3b877af675070fc837
Jun4928 <jun.choi.4928@gmail.com>
2024-02-22 16:05:31 +0100

    feat: 217 contains duplicate

---
+++ leetcode/hashing/contains-duplicate.js
@@ -0,0 +1,10 @@
+/**
+https://leetcode.com/problems/contains-duplicate/description/
+217. Contains Duplicate
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+  return new Set(nums).size !== nums.length
+}
+x
